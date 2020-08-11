9f96fd6979e8a3a8e69c2330ebdcc7b7a70fec33
Ambrala <ambralaju@gmail.com>
2020-08-11 17:19:57 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+let bugs = [];
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 
@@ -9,14 +11,38 @@ function setup() {
   canvas.position(0,0);
   canvas.style('z-index','-1');
     img = new Image();
-    img.src = "lib/pic/day01.png";
+    img.src = "lib/pic/me07.png";
+	
+	for (let i = 0; i < 20; i++) {
+    bugs.push(new Jitter());
+  }
 }
 
 function draw() {
 	background('#99CCFF');
-    drawingContext.drawImage(img,0,290, img.width / 10, img.height / 10);
+	
+	for (let i = 0; i < bugs.length; i++) {
+    bugs[i].move();
+    bugs[i].display();
+  }
 }
 
+class Jitter {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.diameter = random(50, 80);
+    this.speed = 1;
+  }
 
+  move() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  display() {
+    drawingContext.drawImage(img,this.x, this.y, this.diameter, this.diameter);
+  }
+}
 
 
